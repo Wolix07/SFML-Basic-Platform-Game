50ea09d5d73161ada70f35dfbd3e6e98fbfb1078
Wojciech Rak <wolix@macbook-air-wojciech.local>
2018-04-03 15:42:27 +0200

    Initial Commit

---
+++ SFML platform/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SFML platform
+//
+//  Created by Wojciech Rak on 03.04.2018.
+//  Copyright © 2018 Wojciech Rak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
